a47c5c72b7dd78eb84239e55a47a2387ea99d826
kirana <leirisayajnakiranafagan@gmail.com>
2024-11-26 12:57:21 +0700

    sign up pengguna

--- finalcode.py
+++ finalcode.py
@@ -22,6 +22,34 @@ def save_user_data(name, username, email, password):
     messagebox.showinfo("Success", "Pendaftaran akun berhasil! Silakan login kembali.")
     show_login_frame()
 
-print('')
-print('halloo')
-print("haii")
+# Pendaftaran pengguna (KIRANA)
+def register_user(name_entry, username_entry, email_entry, password_entry):
+    name = name_entry.get()
+    username = username_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    # Validasi input
+    if not name or not username or not email or not password:
+        messagebox.showerror("Error", "Semua kolom wajib diisi.")
+        return
+    if len(password) < 8:
+        messagebox.showerror("Error", "Password harus memiliki minimal 8 karakter.")
+        return
+    
+    # Memeriksa data pengguna
+    try:
+        with open('user_data.csv', 'r', newline='') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                if row[1] == username:
+                    messagebox.showerror("Error", "Username telah digunakan.")
+                    return
+                if row[2] == email:
+                    messagebox.showerror("Error", "Email telah digunakan.")
+                    return
+    except FileNotFoundError:
+        create_user_table()
+    
+    save_user_data(name, username, email, password)
